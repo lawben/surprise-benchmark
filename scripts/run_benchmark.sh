80621a23f504fc61f0b16bd07cbda76e6a14fb7f
Lawrence Benson <github@lawben.com>
2023-11-15 14:36:19 +0100

    Clean up logging a bit

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -5,7 +5,7 @@ set -e
 # Create TPC-H tables, load data, and do a trial run to see that TPC-H works.
 echo "Setting up TPC-H..."
 java -jar /benchbase/benchbase.jar -b tpch -c config/init_tpch_config.xml -d /results --create=true --load=true --execute=true
-echo "\n\n\n\n"
+echo -e "\n\n\n\n"
 
 # Run surprise benchmark with unknown queries.
 echo "Running SURPRISE BENCHMARK..."
