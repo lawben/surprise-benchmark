78187caa5982500b4fbdbfee62c8b6dd7889acf3
Lawrence Benson <github@lawben.com>
2023-11-15 15:44:00 +0100

    Setup CI

---
+++ scripts/common.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -e
+
+# Annoying way to get path because OSX does not have `realpath`
+SCRIPT_DIR=$( cd "$(dirname "$0")" ; pwd -P )
+ROOT_DIR=$(dirname "$SCRIPT_DIR")
+echo "Project root dir: $ROOT_DIR"
+
+ALL_SYSTEMS=(postgres mysql)
+SYSTEMS=$@
+if [ $# -eq 0 ]
+then
+    echo "Using all systems"
+    SYSTEMS=("${ALL_SYSTEMS[@]}")
+fi
+echo "Running benchmarks for systems: ("${SYSTEMS[@]}")"
+
+
+export SURPRISE_COMMON=1
+export ROOT_DIR
+export SYSTEMS
---
+++ scripts/run.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Annoying way to get path because OSX does not have `realpath`
+SCRIPT_DIR=$( cd "$(dirname "$0")" ; pwd -P )
+source $SCRIPT_DIR/common.sh $@
+
+cd $ROOT_DIR
+
+echo "Running benchmarks on all systems"
+for system in $SYSTEMS
+do
+    echo "Benchmarking system: $system"
+    docker run \
+	    --rm \
+            -v $ROOT_DIR/results/$system:/results \
+            -v $ROOT_DIR/data/surprise_tpch_queries.xml:/benchbase/config/surprise_tpch_queries.xml:ro \
+            -v $ROOT_DIR/scripts/run_benchmark.sh:/scripts/run_benchmark.sh:ro \
+            --ulimit memlock=1073741824 \
+            --cap-add=sys_nice \
+            surprise-benchmark-$system
+done
+
