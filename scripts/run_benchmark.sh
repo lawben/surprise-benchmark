700b1595190863b96cb499a2c35e00b2aae17d3c
Lawrence Benson <github@lawben.com>
2023-11-21 17:34:27 +0100

    Use custom SQL for Clickhouse

--- scripts/run_benchmark.sh
+++ scripts/run_benchmark.sh
@@ -2,9 +2,19 @@
 
 set -e
 
-# Create TPC-H tables, load data, and do a trial run to see that TPC-H works.
+# Create TPC-H tables and load data.
 echo "Setting up TPC-H..."
-java -jar /benchbase/benchbase.jar -b tpch -c config/init_tpch_config.xml -d /results --create=true --load=true --execute=true
+java -jar /benchbase/benchbase.jar -b tpch -c config/init_tpch_config.xml -d /results --create=true --load=true --execute=false
+echo -e "\n\n\n\n"
+
+# Run to see that TPC-H works.
+echo "Running standard TPC-H..."
+if [ -f "config/run_tpch_config.xml" ]; then
+    java -jar /benchbase/benchbase.jar -b templated -c config/run_tpch_config.xml -d /results --create=false --load=false --execute=true
+else
+    java -jar /benchbase/benchbase.jar -b tpch -c config/init_tpch_config.xml -d /results --create=false --load=false --execute=true
+fi
+
 echo -e "\n\n\n\n"
 
 # Run surprise benchmark with unknown queries.
