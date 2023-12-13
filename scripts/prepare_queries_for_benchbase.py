e2d992614a028c256480b9ad42fb442d17e80251
Jan-Micha Bodensohn <jan-micha.bodensohn@cs.tu-darmstadt.de>
2023-12-13 13:22:25 +0100

    Update surprise_tpch_config.xml files with queries.

--- scripts/prepare_queries_for_benchbase.py
+++ scripts/prepare_queries_for_benchbase.py
@@ -1,6 +1,7 @@
 import argparse
 import os
 import pathlib
+import re
 import shutil
 
 
@@ -10,7 +11,7 @@ def save_queries(queries: list[str], queries_dir: pathlib.Path) -> None:
             file.write(query)
 
 
-def save_queries_for_benchbase(queries: list[str], xml_file: pathlib.Path) -> None:
+def update_xml_file(queries: list[str], xml_file: pathlib.Path) -> None:
     with open(xml_file, "w", encoding="utf-8") as file:
         file.write("""<?xml version="1.0" encoding="UTF-8"?>\n<templates>\n""")
         for ix, query in enumerate(queries):
@@ -18,10 +19,31 @@ def save_queries_for_benchbase(queries: list[str], xml_file: pathlib.Path) -> No
         file.write("""</templates>""")
 
 
+def update_system_xml_files(queries: list[str], system_xml_files: list[pathlib.Path]) -> None:
+    for system_xml_file in system_xml_files:
+        with open(system_xml_file, "r", encoding="utf-8") as file:
+            content = file.read()
+
+        content = re.sub(
+            "<weights>.*</weights>",
+            "<weights>" + ",".join("1" for _ in queries) + "</weights>",
+            content
+        )
+        content = re.sub(
+            r"<transactiontypes>(.|\s)*</transactiontypes>",
+            "<transactiontypes>" + "".join(f"<transactiontype><name>S{ix}</name></transactiontype>" for ix in range(len(queries))) + "</transactiontypes>",
+            content
+        )
+
+        with open(system_xml_file, "w", encoding="utf-8") as file:
+            file.write(content)
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog="prepare_queries_for_benchbase", description="Prepare the generated queries for BenchBase.")
     parser.add_argument("queries_dir", help="queries directory", type=pathlib.Path)
     parser.add_argument("xml_file", help="xml file path", type=pathlib.Path)
+    parser.add_argument("system_xml_files", help="comma-separated list of system-specific xml file paths", type=pathlib.Path, nargs="*")
     args = parser.parse_args()
 
     if not args.queries_dir.is_dir():
@@ -38,4 +60,5 @@ if __name__ == "__main__":
     os.makedirs(args.queries_dir, exist_ok=True)
 
     save_queries(queries, args.queries_dir)
-    save_queries_for_benchbase(queries, args.xml_file)
+    update_xml_file(queries, args.xml_file)
+    update_system_xml_files(queries, args.system_xml_files)
