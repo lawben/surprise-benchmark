4e4778ceb1001be7df30928274a6af091618de45
Jigao Luo <luojigao@outlook.com>
2024-02-06 22:24:40 +0100

    Wrongroad1 Varchar10: postgres and monetdb

--- monetdb/setup_database.sql
+++ monetdb/setup_database.sql
@@ -8,16 +8,16 @@ DROP TABLE IF EXISTS customer;
 DROP TABLE IF EXISTS lineitem;
 
 -- Sccsid:     @(#)dss.ddl  2.1.8.1
-CREATE TABLE nation  ( n_nationkey  INTEGER NOT NULL,
+CREATE TABLE nation  ( n_nationkey  CHAR(2) NOT NULL,
                        n_name       CHAR(25) NOT NULL,
-                       n_regionkey  INTEGER NOT NULL,
+                       n_regionkey  CHAR(1) NOT NULL,
                        n_comment    VARCHAR(152));
 
-CREATE TABLE region  ( r_regionkey  INTEGER NOT NULL,
+CREATE TABLE region  ( r_regionkey  CHAR(1) NOT NULL,
                        r_name       CHAR(25) NOT NULL,
                        r_comment    VARCHAR(152));
 
-CREATE TABLE part  ( p_partkey     INTEGER NOT NULL,
+CREATE TABLE part  ( p_partkey     VARCHAR(10) NOT NULL,
                      p_name        VARCHAR(55) NOT NULL,
                      p_mfgr        CHAR(25) NOT NULL,
                      p_brand       CHAR(10) NOT NULL,
@@ -27,31 +27,31 @@ CREATE TABLE part  ( p_partkey     INTEGER NOT NULL,
                      p_retailprice DECIMAL(15,2) NOT NULL,
                      p_comment     VARCHAR(23) NOT NULL );
 
-CREATE TABLE supplier ( s_suppkey     INTEGER NOT NULL,
+CREATE TABLE supplier ( s_suppkey     VARCHAR(10) NOT NULL,
                         s_name        CHAR(25) NOT NULL,
                         s_address     VARCHAR(40) NOT NULL,
-                        s_nationkey   INTEGER NOT NULL,
+                        s_nationkey   CHAR(2) NOT NULL,
                         s_phone       CHAR(15) NOT NULL,
                         s_acctbal     DECIMAL(15,2) NOT NULL,
                         s_comment     VARCHAR(101) NOT NULL);
 
-CREATE TABLE partsupp ( ps_partkey     INTEGER NOT NULL,
-                        ps_suppkey     INTEGER NOT NULL,
+CREATE TABLE partsupp ( ps_partkey     VARCHAR(10) NOT NULL,
+                        ps_suppkey     VARCHAR(10) NOT NULL,
                         ps_availqty    INTEGER NOT NULL,
                         ps_supplycost  DECIMAL(15,2)  NOT NULL,
                         ps_comment     VARCHAR(199) NOT NULL );
 
-CREATE TABLE customer ( c_custkey     INTEGER NOT NULL,
+CREATE TABLE customer ( c_custkey     VARCHAR(10) NOT NULL,
                         c_name        VARCHAR(25) NOT NULL,
                         c_address     VARCHAR(40) NOT NULL,
-                        c_nationkey   INTEGER NOT NULL,
+                        c_nationkey   CHAR(2) NOT NULL,
                         c_phone       CHAR(15) NOT NULL,
                         c_acctbal     DECIMAL(15,2)   NOT NULL,
                         c_mktsegment  CHAR(10) NOT NULL,
                         c_comment     VARCHAR(117) NOT NULL);
 
-CREATE TABLE orders  ( o_orderkey       INTEGER NOT NULL,
-                       o_custkey        INTEGER NOT NULL,
+CREATE TABLE orders  ( o_orderkey       VARCHAR(10) NOT NULL,
+                       o_custkey        VARCHAR(10) NOT NULL,
                        o_orderstatus    CHAR(1) NOT NULL,
                        o_totalprice     DECIMAL(15,2) NOT NULL,
                        o_orderdate      DATE NOT NULL,
@@ -60,9 +60,9 @@ CREATE TABLE orders  ( o_orderkey       INTEGER NOT NULL,
                        o_shippriority   INTEGER NOT NULL,
                        o_comment        VARCHAR(79) NOT NULL);
 
-CREATE TABLE lineitem ( l_orderkey    INTEGER NOT NULL,
-                        l_partkey     INTEGER NOT NULL,
-                        l_suppkey     INTEGER NOT NULL,
+CREATE TABLE lineitem ( l_orderkey    VARCHAR(10) NOT NULL,
+                        l_partkey     VARCHAR(10) NOT NULL,
+                        l_suppkey     VARCHAR(10) NOT NULL,
                         l_linenumber  INTEGER NOT NULL,
                         l_quantity    DECIMAL(15,2) NOT NULL,
                         l_extendedprice  DECIMAL(15,2) NOT NULL,
